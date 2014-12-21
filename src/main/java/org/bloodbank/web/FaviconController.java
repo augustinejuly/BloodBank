e89c162728d11b3abe4476ed08df9237ee1c23d2
Augustine <augustinejuly@gmail.com>
2014-12-22 02:38:10 +0530

    handled favicon.con issue

---
+++ src/main/java/org/bloodbank/web/FaviconController.java
@@ -0,0 +1,13 @@
+package org.bloodbank.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FaviconController {
+	
+    @RequestMapping("favicon.ico")
+    @ResponseBody
+    public void favicon() {}
+}
